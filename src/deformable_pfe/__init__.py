55578ebed99bafd79cd6cf6cb0e76f8a493b5f15
Prarthana <p6bhatta@uwaterloo.ca>
2020-08-26 00:59:25 -0400

    Changes to deformable_pfe

--- src/deformable_pfe/__init__.py
+++
@@ -1,9 +0,0 @@
-from .voxel_set_abstraction import VoxelSetAbstraction
-from .def_voxel_set_abstraction import DefVoxelSetAbstraction
-
-
-__all__ = {
-    'VoxelSetAbstraction': VoxelSetAbstraction
-    'DefVoxelSetAbstraction': DefVoxelSetAbstraction
-}
-
