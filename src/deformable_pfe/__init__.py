7e153523d7149463001ab1a3eb6e495936f9730f
Prarthana <p6bhatta@uwaterloo.ca>
2020-08-26 00:51:16 -0400

    Add deformable_pfe

---
+++ src/deformable_pfe/__init__.py
@@ -0,0 +1,9 @@
+from .voxel_set_abstraction import VoxelSetAbstraction
+from .def_voxel_set_abstraction import DefVoxelSetAbstraction
+
+
+__all__ = {
+    'VoxelSetAbstraction': VoxelSetAbstraction
+    'DefVoxelSetAbstraction': DefVoxelSetAbstraction
+}
+
